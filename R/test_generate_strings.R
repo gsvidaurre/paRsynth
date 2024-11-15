9d1e5274be7fec1059388b46c954e97dfec0f698
Grethel Alexandra Juarez <juarezgr@msu.edu>
2024-11-15 11:16:03 -0500

    generate_string Unit Test A

--- R/test_generate_strings.R
+++
@@ -1,9 +0,0 @@
-# G.A. Juarez
-# 11 Nov 2024
-
-rm(list = ls())
-
-if (!require(testthat)) install.packages('testthat')
-library(testthat)
-
-source("/home/gsvidaurre/Desktop/GitHub_repos/ABISSMAL/R/detect_clusters.R")
