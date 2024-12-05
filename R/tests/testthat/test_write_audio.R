b64fbc90d5e6e03b39a0c1674ae95b284e6655f8
Grethel Alexandra Juarez <juarezgr@msu.edu>
2024-12-05 11:21:56 -0500

    Update test_write_audio.R

--- R/tests/testthat/test_write_audio.R
+++ R/tests/testthat/test_write_audio.R
@@ -1,22 +1,44 @@
 # R. Samman
 # Nov, 18, 2024
+# G.A. Juarez
+# 5Dec24 - Grammar check
 
-source("/Users/raneemsamman/Documents/GitHub/paRsynth/R/write_audio.R")
+rm(list = ls())
+
+if (!require(testthat)) install.packages('testthat')
 library(testthat)
-desktop_path <- "/Users/raneemsamman/Desktop"
+
+# Change "~Desktop/.../GitHub_repos" based on where paRsynth is stored
+source("~/Desktop/BIRDS/GitHub_repos/paRsynth/R/write_audio.R")
+
+desktop_path <- "~/Desktop"
 
 # Unit Test 1: Test for correct audio file creation
 test_that("Audio files are correctly created and removed", {
-  
+
+  # Avoid library calls and other changes to the virtual environment
+  # See https://r-pkgs.org/testing-design.html
+  withr::local_package("tidyverse")
+  withr::local_package("dplyr")
+  withr::local_package("lubridate")
+  withr::local_package("soundgen")
+
+  # Just for code development
+  # library(tidyverse)
+  # library(lubridate)
+  # library(testthat)
+  # library(dplyr)
+  # library(soundgen)
+
   # Create a temporary directory on the Desktop for storing files (for test purposes)
   tmp_dir <- file.path(desktop_path, "R_test_temp")
-  
+
   # Create the directory if it doesn't already exist
   if (!dir.exists(tmp_dir)) {
     dir.create(tmp_dir)
   }
   print(tmp_dir)
-  
+
   # Example data frame for testing
   df_test <- data.frame(
     Group = c(1, 2),
@@ -40,17 +62,32 @@ test_that("Audio files are correctly created and removed", {
 })
 
 
-# Unit Test 2: Test for correct df creation (must add an additional column -audio_file_name- for file names with a correct naming format) 
-test_that("Data frame contains correct audio file name format", {  
+# Unit Test 2: Test for correct df creation (must add an additional column -audio_file_name- for file names with a correct naming format)
+test_that("Data frame contains correct audio file name format", {
+
+  # Avoid library calls and other changes to the virtual environment
+  # See https://r-pkgs.org/testing-design.html
+  withr::local_package("tidyverse")
+  withr::local_package("dplyr")
+  withr::local_package("lubridate")
+  withr::local_package("soundgen")
+
+  # Just for code development
+  # library(tidyverse)
+  # library(lubridate)
+  # library(testthat)
+  # library(dplyr)
+  # library(soundgen)
+
   # Create a temporary directory on the Desktop for storing files (for test purposes)
   tmp_dir <- file.path(desktop_path, "R_test_temp")
-  
+
   # Create the directory if it doesn't already exist
   if (!dir.exists(tmp_dir)) {
     dir.create(tmp_dir)
   }
   print(tmp_dir)
-  
+
   df_test <- data.frame(
     Group = c(1, 2),
     Individual = c(1, 2),
@@ -62,7 +99,7 @@ test_that("Data frame contains correct audio file name format", {
 
   # testing if the audio_file_name column exists and filenames are correctly formatted
   expect_true("audio_file_name" %in% colnames(result_df))
-  
+
   # Check the naming format for the first row
   expected_filename <- paste0("TestPrefix_Group", df_test$Group[1], "_Ind", df_test$Individual[1], "_Call", df_test$Call[1], ".wav")
   expect_equal(result_df$audio_file_name[1], expected_filename)
@@ -73,10 +110,24 @@ test_that("Data frame contains correct audio file name format", {
 
 # Unit Test 3: Test whether the resulting files contain the correct file extentsion
 test_that("Generated files have the correct .wav extension", {
-  
+
+  # Avoid library calls and other changes to the virtual environment
+  # See https://r-pkgs.org/testing-design.html
+  withr::local_package("tidyverse")
+  withr::local_package("dplyr")
+  withr::local_package("lubridate")
+  withr::local_package("soundgen")
+
+  # Just for code development
+  # library(tidyverse)
+  # library(lubridate)
+  # library(testthat)
+  # library(dplyr)
+  # library(soundgen)
+
   # Create a temporary directory on the Desktop for storing files (for test purposes)
   tmp_dir <- file.path(desktop_path, "R_test_temp")
-  
+
   # Create the directory if it doesn't already exist
   if (!dir.exists(tmp_dir)) {
     dir.create(tmp_dir)
